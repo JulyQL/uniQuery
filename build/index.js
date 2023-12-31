bf7ed464ea4e2f5f5a01b9e0a399c9657941f2d9
RU <liulq@redoornetwork.com>
2023-12-31 12:49:23 +0800

    调整文档UI+新增API和form组件+添加微信公众号和小程序必要说明

--- build/index.js
+++ build/index.js
@@ -1,10 +1,8 @@
-const { copySync } = require("devby");
+const { copySync, deleteSync, error } = require("devby");
 const package = require("../package.json");
 const fs = require("fs");
 
 // 在线用例
-copySync("./unpackage/dist/build/h5", "./docs/examples");
-
 fs.writeFileSync("./docs/js/system.js", `window.uniQuery_system = {
     "version": "${package.version}"
 };`);
